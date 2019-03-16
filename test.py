f5d0eaf1822c0c1957cd3b23d4d556c8488d1fb7
Pedro Teixeira <pedro.h.d.teixeira@gmail.com>
2019-03-16 18:05:23 +0000

    added funcs to send messages, pretty print and login

--- test.py
+++ test.py
@@ -1,22 +1,38 @@
 import json
 import socket
 
-json_str = json.dumps({"type":"login", "player":"moneymaker", "tournament":False})
-
+# Server details
 TCP_IP = '35.197.236.148'
 TCP_PORT = 9877
 
-MESSAGE = bytearray(len(json_str).to_bytes(4, "little")) + bytearray(json_str, 'utf-8')
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-response_len = s.recv(4)
-BUFFER_SIZE = int.from_bytes(response_len, byteorder='little')
-data = s.recv(BUFFER_SIZE)
+"""Sends the given message to the game server and returns the response as a JSON object"""
+def speak(msg):
 
-s.close()
+    encoded_msg= bytearray(len(msg).to_bytes(4, "little")) + bytearray(msg, 'utf-8')
 
-response = json.loads(data)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((TCP_IP, TCP_PORT))
+        s.send(encoded_msg)
+        response_len = s.recv(4)
+        BUFFER_SIZE = int.from_bytes(response_len, byteorder='little')
+        data = s.recv(BUFFER_SIZE)
 
-print(json.dumps(response, indent=4, separators=(',', ': ')))
+    return json.loads(data)
+
+
+"""Log us into the game server """
+def login(tournament=False):
+
+    json_str = json.dumps({"type": "login", "player": "moneymaker", "tournament": tournament})
+
+    return speak(json_str)
+
+
+"""Converts JSON object to pretty str ready for printing"""
+def pretty_json(data):
+
+     return json.dumps(data, indent=4, separators=(',', ': '))
+
+
+print(pretty_json(login()))
